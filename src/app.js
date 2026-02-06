f7d6b1d2207b5a6809c62817b3c4df29f175d479
danilox100 <nascimentod792@gmail.com>
2026-02-06 18:28:24 -0300

    primeiro-commit

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import routes from './routes.js';
+import fileRoutesConfig from './config/fileRoutes.cjs';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(routes);
+app.use('/product-file', fileRoutesConfig);
+
+export default app;
